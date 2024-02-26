587a7c2dfff15909841c4e7991bcafa455449ede
NathanMacktravis <95182428+NathanMacktravis@users.noreply.github.com>
2024-02-27 00:49:06 +0100

    model training  stage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-"""
+
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -26,8 +26,6 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-"""
 
 
 
